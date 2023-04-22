177cc14347d72186acce18506c211070b9ce5377
ngatti68 <norberto.o.gatti@gmail.com>
2023-04-22 16:01:01 -0300

    CORSConfig

---
+++ src/main/java/com/miportfolioweb/backend/CORSConfig.java
@@ -0,0 +1,22 @@
+package com.miportfolioweb.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer CORSConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://ng-portfolioweb-ap.web.app")
+                        .allowedMethods("GET", "POST", "PUT","DELETE");
+            }
+        };
+    }
+
+}
+
